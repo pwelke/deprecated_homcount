64893f82a178b8d3b842c9fad1f0486895bd31c0
Pascal Welke <welke@uni-bonn.de>
2023-01-17 12:09:17 +0100

    add filter for messed up homcounts and rename

---
+++ convert_to_homdensity.py
@@ -0,0 +1,45 @@
+import sys
+sys.path.append('graph-homomorphism-network/src')
+from ghc.utils.data import load_json, save_json
+import json
+import itertools
+
+datasets = ['ogbg-moltox21',
+            'ogbg-molesol',
+            'ogbg-molbace',
+            'ogbg-molclintox',
+            'ogbg-molbbbp',
+            'ogbg-molsider',
+            'ogbg-moltoxcast',
+            'ogbg-mollipo',
+            'ogbg-molhiv',]
+
+run_ids = ['il3',]# 'il4',] #'run3', 'run4', 'run5', 'run6', 'run7', 'run8', 'run9', 'run10']
+
+pattern_counts = [50,] #[30, ] #10, 50, 100, 200]
+
+hom_types = ['full_kernel']
+
+hom_size = 'max'
+
+dloc = 'forFabian/2023-01-12_fixedreps/'
+
+for run_id, dataset, pattern_count, hom_type in itertools.product(run_ids, datasets, pattern_counts, hom_types):
+
+    try:
+
+        meta = load_json(dataset.upper(), hom_type, hom_size, pattern_count, run_id, dloc)
+
+        pattern_sizes = meta['pattern_sizes']
+        data = meta['data']
+
+        def density(n, counts, sizes):
+            return [c / (n ** s) for c,s in zip(counts, sizes)]
+
+        for x in data:
+            hom_density = density(x['vertices'], x['counts'], pattern_sizes)
+            x['counts'] = hom_density
+
+        save_json(meta, dataset.upper(), hom_type, hom_size, pattern_count, run_id, dloc, suffix='densities')
+    except FileNotFoundError:
+        pass # we don't process whats not there
---
+++ convert_to_overflowfiltered.py
@@ -0,0 +1,63 @@
+import sys
+sys.path.append('graph-homomorphism-network/src')
+from ghc.utils.data import load_json, save_json
+import json
+import itertools
+import numpy as np
+
+datasets = ['ogbg-moltox21',
+            'ogbg-molesol',
+            'ogbg-molbace',
+            'ogbg-molclintox',
+            'ogbg-molbbbp',
+            'ogbg-molsider',
+            'ogbg-moltoxcast',
+            'ogbg-mollipo',
+            'ogbg-molhiv',]
+
+run_ids = ['il3',]# 'il4',] #'run3', 'run4', 'run5', 'run6', 'run7', 'run8', 'run9', 'run10']
+
+pattern_counts = [50,] #[30, ] #10, 50, 100, 200]
+
+hom_types = ['full_kernel']
+
+hom_size = 'max'
+
+dloc = 'forFabian/2023-01-12_fixedreps_more/'
+
+def filter_overflow(patterns, sizes):
+    minval = np.min(patterns, axis=0)
+    patterns = patterns[:, minval >= 0]
+    sizes = sizes[minval >= 0]
+
+    if patterns.shape[1] > 0:
+        return patterns, sizes
+    else: 
+        # if nothing worked, return zeros
+        return np.zeros([patterns.shape[0], 1]), np.zeros(1)
+
+for run_id, dataset, pattern_count, hom_type in itertools.product(run_ids, datasets, pattern_counts, hom_types):
+
+    try:
+
+        meta = load_json(dataset.upper(), hom_type, hom_size, pattern_count, run_id, dloc)
+
+        pattern_sizes = np.array(meta['pattern_sizes'])
+        data = meta['data']
+        features = np.array([x['counts'] for x in data], dtype=float)
+
+        size_before = pattern_sizes.shape[0]
+
+        features, pattern_sizes = filter_overflow(features, pattern_sizes)
+
+        size_after = pattern_sizes.shape[0]
+
+        meta['pattern_sizes'] = pattern_sizes.tolist()
+        for x, f in zip(data, features):
+            x['counts'] = f.tolist()
+
+        print(f'{dataset} {size_before}->{size_after}, min={np.min(features)}')
+
+        save_json(meta, dataset.upper(), hom_type, hom_size, pattern_count, run_id, dloc, suffix='overflow_filtered')
+    except FileNotFoundError:
+        pass # we don't process whats not there
