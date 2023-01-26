33c9e4efd1a915c2eb3d3f7a78fa8b9b01dde3ce
Pascal Welke <welke@uni-bonn.de>
2023-01-26 20:47:03 +0100

    remove some obsolete files

--- result_parsing.py
+++ result_parsing.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import numpy as np
 
-infiles = ['2022-10-19.csv', '2022-10-20.csv']
+
+# parse and pretty print some results
+infiles = ['2022-12-20_as-wl3.csv']
+    
 
 for infile in infiles:
     df = pd.read_csv(infile, header=None, delimiter=' ')
-    print(df.groupby([5,6], ).agg(mean=(8, np.mean), std=(8, np.std)).to_latex())
+    print(df.groupby([5,10], ).agg(mean=(8, np.mean), std=(8, np.std)).to_latex())
