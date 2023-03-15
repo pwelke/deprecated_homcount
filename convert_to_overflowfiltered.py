53ae21064fc99c053d2c1d190e48a0184dd2a28b
Pascal Welke <welke@uni-bonn.de>
2023-03-15 14:39:24 +0100

    add removal of duplicate patterns on 1,2,3 vertices

--- convert_to_overflowfiltered.py
+++ convert_to_overflowfiltered.py
@@ -17,6 +17,21 @@ def filter_overflow(patterns, sizes):
         # if nothing worked, return zeros
         return np.zeros([patterns.shape[0], 1]), np.zeros(1)
 
+def filter_singletons(patterns, sizes):
+    larger = (sizes > 2)
+    # first column contains the singleton counts that we want to keep
+    larger[0] = True
+    # second column contains the single edge counts that we want to keep
+    larger[1] = True
+    patterns = patterns[:, larger]
+    sizes = sizes[larger]
+
+    if patterns.shape[1] > 0:
+        return patterns, sizes
+    else: 
+        # if nothing worked, return zeros
+        return np.zeros([patterns.shape[0], 1]), np.zeros(1)
+
 
 def file_overflow_filter(run_ids, datasets, pattern_counts, hom_types):
     for run_id, dataset, pattern_count, hom_type in itertools.product(run_ids, datasets, pattern_counts, hom_types):
@@ -45,17 +60,44 @@ def file_overflow_filter(run_ids, datasets, pattern_counts, hom_types):
             pass
 
 
+def file_singleton_filter(run_ids, datasets, pattern_counts, hom_types):
+    for run_id, dataset, pattern_count, hom_type in itertools.product(run_ids, datasets, pattern_counts, hom_types):
+
+        try:
+            meta = load_json(dataset.upper(), hom_type, hom_size, pattern_count, run_id, dloc, suffix='overflow_filtered')
+
+            pattern_sizes = np.array(meta['pattern_sizes'])
+            data = meta['data']
+            features = np.array([x['counts'] for x in data], dtype=float)
+
+            size_before = pattern_sizes.shape[0]
+
+            features, pattern_sizes = filter_singletons(features, pattern_sizes)
+
+            size_after = pattern_sizes.shape[0]
+
+            meta['pattern_sizes'] = pattern_sizes.tolist()
+            for x, f in zip(data, features):
+                x['counts'] = f.tolist()
+
+            print(f'{dataset} {size_before}->{size_after}, min={np.min(features)}')
+
+            save_json(meta, dataset.upper(), hom_type, hom_size, pattern_count, run_id, dloc, suffix='singleton_filtered')
+        except FileNotFoundError:
+            pass
+
+
 if __name__ == '__main__':
 
-    run_ids = ['20230117']
+    run_ids = ['run' + str(i+1) for i in range(9)]
 
-    pattern_counts = [-5] 
+    pattern_counts = [50] 
 
     hom_types = ['full_kernel']
 
     hom_size = 'max'
 
-    dloc = 'forFabian/2023-01-12_fixedreps_more/'
+    dloc = '2023-03-07_ogbpyg_repetition/repeated_runs/'
 
     datasets = ['ogbg-moltox21',
                 'ogbg-molesol',
@@ -69,3 +111,4 @@ if __name__ == '__main__':
                 'ZINC_subset']
 
     file_overflow_filter(run_ids, datasets, pattern_counts, hom_types)
+    file_singleton_filter(run_ids, datasets, pattern_counts, hom_types)
