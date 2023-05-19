4f13a8941b17fc3617d09fa50f4fb7ebcf9537c4
Pascal Welke <welke@uni-bonn.de>
2023-05-19 09:57:13 +0200

    rename and merge compute_ scripts

--- compute_dummypattern.py
+++ compute_dummypattern.py
@@ -2,12 +2,11 @@ import subprocess
 import itertools
 import sys
 import os
-import hashlib
+import hashlib 
+
 
 # parameters to iterate over
 cwd = './'
-dloc = 'graph-homomorphism-network/data/'
-
 
 datasets = ['ogbg-moltox21',
             'ogbg-molesol',
@@ -17,37 +16,29 @@ datasets = ['ogbg-moltox21',
             'ogbg-molsider',
             'ogbg-moltoxcast',
             'ogbg-mollipo',
-            'ogbg-molhiv',
-            'ZINC_subset']
+            'ogbg-molhiv',]
 
-executables = ['graph-homomorphism-network/models/svm.py', 'graph-homomorphism-network/models/mlp.py'] 
+executables = ['graph-homomorphism-network/models/pattern_debugging.py', ] 
 
-run_ids = ['run1', 'run2','run3', 'run4', 'run5', 'run6', 'run7', 'run8', 'run9', 'run10']
+run_ids = ['il3', 'il4',] #'run3', 'run4', 'run5', 'run6', 'run7', 'run8', 'run9', 'run10']
 
 pattern_counts = [50] 
 
 hom_types = ['full_kernel']
 
-hom_size = 'max'
-
-
 # a deterministic hash function returning a 32 bit integer value for a given utf-8 string
 hashfct = lambda x: str(int(hashlib.sha1(bytes(x, 'utf-8')).hexdigest(), 16) & 0xFFFFFFFF)
 
-
 for run_id, dataset, executable, pattern_count, hom_type in itertools.product(run_ids, datasets, executables, pattern_counts, hom_types):
-    print(f'{run_id}: {dataset} {executable}')
+    print(f'{run_id}: {dataset} {executable}')    
     args = ['python', executable, 
             '--data', dataset,
             '--seed', hashfct(run_id),
-            '--dloc', dloc,
+            '--dloc', 'graph-homomorphism-network/data',
             '--pattern_count', str(pattern_count),
             '--run_id', run_id,
             '--hom_type', hom_type,
             '--hom_size', '-1',
-            '--grid_search',
             ]
+    print(args)
     subprocess.run(args, cwd=cwd, stdout=sys.stdout, stderr=sys.stderr, check=True)
-
-file_overflow_filter(run_ids, datasets, pattern_counts, hom_types, hom_size, dloc + 'precompute')
-file_singleton_filter(run_ids, datasets, pattern_counts, hom_types, hom_size, dloc + 'precompute')
